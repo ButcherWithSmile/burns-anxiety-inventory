77b8eccd7020ce4759566be78933b9c62206b147
Hatef PourRajabi <131672914+ButcherWithSmile@users.noreply.github.com>
2023-09-26 17:19:00 +0330

    Update checkInput.go

--- CheckInput/checkInput.go
+++ CheckInput/checkInput.go
@@ -1,3 +1,5 @@
+// This package imports the necessary libraries for reading user input and converting it to an integer
+
 package checkInput
 
 import (
@@ -5,13 +7,18 @@ import (
 	"strconv"
 )
 
+// This function prompts the user to enter a number and stores it in the variable x
 func Check() int {
 	var x string
 	fmt.Scanln(&x)
 
+	// This code attempts to convert the string x to an integer
+	// If the conversion is successful and the integer is between 0 and 3, the value of y is set to the converted integer
+	// Otherwise, the value of y is set to 0
 	y, err := strconv.Atoi(x)
 	if err != nil || y < 0 || y > 3 {
 		y = 0
 	}
+	// This function returns the value of y
 	return y
 }
