b7d1b78c36317d2c57f5c7dca91991d744500870
Hatef PourRajabi <131672914+ButcherWithSmile@users.noreply.github.com>
2023-09-26 17:30:51 +0330

    Update worryingThoughts.go

--- WorryingThoughts/worryingThoughts.go
+++ WorryingThoughts/worryingThoughts.go
@@ -1,3 +1,5 @@
+// This package imports the necessary libraries for printing text to the console, getting user input, and converting strings to integers
+
 package worryingThoughts
 
 import (
@@ -6,10 +8,12 @@ import (
 	"strings"
 )
 
+// This constant stores the options that are presented to the user and the separator that is used to print the options
 const options = "0: Not at all | 1: A little | 2: Partially | 3: Very much"
 
 var separator = strings.Repeat("_", 35)
 
+// This function prints the title and separator for the Worrying Thoughts section of the Burns Anxiety Inventory
 func WorryingThoughts() int {
 	t.Println("Group 2: Worrying thoughts")
 	t.Println(separator)
